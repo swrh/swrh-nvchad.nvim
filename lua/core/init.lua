c0d2b4097b3a7a86b599825a69f9b2fffbe5e0a6
Fernando Silveira <fsilveira@gmail.com>
2023-09-16 14:58:16 -0300

    Initial commit

---
+++ lua/core/init.lua
@@ -0,0 +1,14 @@
+-- lua/core/init.lua
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+vim.opt.number = true
+vim.opt.numberwidth = 2
+vim.opt.ruler = true
+
+vim.opt.list = true
+vim.opt.listchars = 'tab:>.,trail:.,eol:$'

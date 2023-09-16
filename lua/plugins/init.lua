0c0e44f5a9261448d8b5fa66492e144952762130
Fernando Silveira <fsilveira@gmail.com>
2023-09-16 16:28:06 -0300

    Set up which-key

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,6 +3,17 @@ local config = {
 }
 
 local plugins = {
+  {
+    'folke/which-key.nvim',
+    keys = { '<Leader>', '<C-r>', '"', '\'', '`', 'c', 'v', 'g' },
+    init = function()
+      require('core.utils').load_mappings('whichkey')
+    end,
+    cmd = 'WhichKey',
+    config = function(_, opts)
+      require('which-key').setup(opts)
+    end,
+  },
 }
 
 require('lazy').setup(plugins, config)

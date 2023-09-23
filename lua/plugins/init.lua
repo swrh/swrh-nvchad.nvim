7229eb53df268037c9ca8debe5d52572fd9e9d17
Fernando Silveira <fsilveira@gmail.com>
2023-09-23 08:18:22 -0300

    Remove `MasonInstallAll` command

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -59,18 +59,12 @@ local plugins = {
 
   {
     'williamboman/mason.nvim',
-    cmd = { 'Mason', 'MasonInstall', 'MasonInstallAll', 'MasonUninstall', 'MasonUninstallAll', 'MasonLog' },
+    cmd = { 'Mason', 'MasonInstall', 'MasonUninstall', 'MasonUninstallAll', 'MasonLog' },
     opts = function()
       return require('plugins.configs.mason')
     end,
     config = function(_, opts)
       require('mason').setup(opts)
-
-      vim.api.nvim_create_user_command('MasonInstallAll', function()
-        vim.cmd('MasonInstall ' .. table.concat(opts.ensure_installed, ' '))
-      end, {})
-
-      vim.g.mason_binaries_list = opts.ensure_installed
     end,
   },
 
